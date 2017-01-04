20a6f6a2e1156c1e83861604738dfd8de35fd220
Roberto Huesca <robertohuesca@outlook.com>
2017-01-04 15:25:11 -0600

    Versión mejorada con WebView

--- app/src/main/java/com/example/enduser/procig/Sax.java
+++ app/src/main/java/com/example/enduser/procig/Sax.java
@@ -1,14 +1,40 @@
 package com.example.enduser.procig;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Sax extends AppCompatActivity {
+    private WebView browser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sax);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        browser = (WebView)findViewById(R.id.webview);
+
+        //habilitamos javascript y el zoom
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.getSettings().setBuiltInZoomControls(true);
+
+        //habilitamos los plugins (flash)
+
+
+
+        browser.loadUrl("http://saxsoft.com.mx");
+
+        browser.setWebViewClient(new WebViewClient()
+        {
+            // evita que los enlaces se abran fuera nuestra app en el navegador de android
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                return false;
+            }
+
+        });
     }
 }
